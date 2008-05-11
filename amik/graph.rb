151d4023f83073b33d8d951d00809ad5450a7c3f
James Bowes <jbowes@dangerouslyinc.com>
2008-05-11 10:20:39 -0400

    Draw graph on a gtk widget

--- amik/graph.rb
+++ amik/graph.rb
@@ -17,21 +17,40 @@
 # Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
 require 'cairo'
+require 'gtk2'
+
 require 'amik/logger'
 
 $log = get_logger()
 
-class Graph
+
+WHITE = Gdk::Color.new(65535, 65535, 65535)
+BLACK = Gdk::Color.new(0, 0, 0)
+BLUE = Gdk::Color.new(0, 0, 65535)
+
+class Graph < Gtk::DrawingArea
+
+    # Register with gobject
+    type_register
 
     def initialize
-        @surface = Cairo::ImageSurface.new(400, 300)
-        @context = Cairo::Context.new(@surface)
-        @context.set_source_color(:white)
+        super
+        set_size_request(400, 300)
+        signal_connect("expose_event") { expose_event }
+    end
+
+    def expose_event
+        @context = window.create_cairo_context
+        @context.set_source_color(WHITE)
         @context.paint
+
+        draw
+
+        true
     end
 
     def draw_grid
-        @context.set_source_color(:black)
+        @context.set_source_color(BLACK)
         (1..9).each do |i|
             @context.move_to(400/10 * i, 0)
             @context.line_to(400/10 * i, 300)
@@ -52,7 +71,7 @@ class Graph
     end
 
     def graph_points(points)
-        @context.set_source_color(:blue)
+        @context.set_source_color(BLUE)
         first_point = transform_point(points[0])
         @context.move_to(first_point[0], first_point[1])
         points.each do |point|
@@ -75,12 +94,18 @@ class Graph
         graph_points([[1,1], [1,2], [2, 3]])
     end
 
-    def write
-        @surface.write_to_png("output.png")
-    end
-
 end
 
-graph = Graph.new
-graph.draw
-graph.write
+if __FILE__ == $0
+    window = Gtk::Window.new('Graph Test')
+    window.signal_connect("delete-event") do
+        Gtk.main_quit
+    end
+
+    graph = Graph.new
+
+    window.add(graph)
+    window.show_all
+
+    Gtk.main
+end
